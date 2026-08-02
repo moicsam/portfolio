9a29e915abf99377d118eb0e55ac636cefbcb357
Samuel Klein-Bichain <samuel.kleinbichain@proton.me>
2026-08-02 13:49:29 -0400

    add: portfolio template

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Un flake pour installer npm dans un environnement de développement";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.nodejs ];
+      };
+    };
+}
